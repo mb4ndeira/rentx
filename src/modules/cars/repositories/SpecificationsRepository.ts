730447a779b1365186d0dd626893d927a22cbbcf
mb4ndeira <mbandeira.cs@gmail.com>
2021-08-25 21:56:36 -0300

    feat: add specification repository and interface

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,26 @@
+import { Specification } from '../model/Specification'
+import { ISpecificationsRepository, ICreateSpecificationDTO } from './ISpecificationsRepository'
+
+class SpecificationsRepository implements ISpecificationsRepository {
+    private specifications: Specification[]
+
+    constructor () {
+      this.specifications = []
+    }
+
+    create ({ description, name }: ICreateSpecificationDTO) {
+      const specification = new Specification()
+
+      Object.assign(specification, { name, description, createdAt: new Date() })
+
+      this.specifications.push(specification)
+    }
+
+    findByName (name: string) {
+      const specification = this.specifications.find(specification => specification.name === name)
+
+      return specification
+    }
+}
+
+export { SpecificationsRepository }
